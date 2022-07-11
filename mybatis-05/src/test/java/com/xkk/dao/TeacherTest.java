1975e1c846ec58500cd48bd3d1d714f2c28b3bb3
xuekaikai <2780739366@qq.com>
2022-07-11 16:20:00 +0800

    多对一处理 association

--- mybatis-05/src/test/java/com/xkk/dao/TeacherTest.java
+++ mybatis-05/src/test/java/com/xkk/dao/TeacherTest.java
@@ -1,9 +1,12 @@
 package com.xkk.dao;
 
+import com.xkk.pojo.Student;
 import com.xkk.pojo.Teacher;
 import com.xkk.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
+
+import java.util.List;
 
 public class TeacherTest {
     @Test
@@ -12,5 +15,18 @@ public class TeacherTest {
         TeacherMapper teacherMapper = sqlSession.getMapper(TeacherMapper.class);
         Teacher teacher = teacherMapper.getTeacherById(1);
         System.out.println(teacher);
+        sqlSession.close();
+    }
+
+    @Test
+    public void getStudent() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+//        List<Student> list = studentMapper.getStudent();
+        List<Student> list = studentMapper.getStudent2();
+        for (Student student : list) {
+            System.out.println(student);
+        }
+        sqlSession.close();
     }
 }
